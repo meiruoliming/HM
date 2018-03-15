430eb283d0a3189eea06d1696e987f8ad03ec0d5
limo <mo025913@163.com>
2018-03-15 22:29:09 +0800

    first

---
+++ js/common.js
@@ -0,0 +1,30 @@
+// 提示框
+function message(msg){
+    var content = '<div class="messagebox" style="display:block;z-index:99999">' +
+        '<h4>提示' +
+        '</h4><p class="">'+msg+'</p>' +
+        '</div>';
+    $("body").after(content);
+    $(".messagebox").fadeOut(2500);
+};
+
+// 获取验证码
+var t;
+var num=60;
+function Sendcode(obj){
+    clearInterval(t);
+    t = setInterval(function () {
+        $(obj).html(num + "秒后重新发送");
+        num--;
+        if (num == 0) {
+           $(obj).html("重新获取验证码");
+            clearInterval(t);
+            num=60;
+        }
+    },1000);
+};
+
+// 返回上一个页面
+// function Back(){
+//      window.history.back(-1);
+//  }
